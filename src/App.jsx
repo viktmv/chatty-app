d2844f99e7e51c524f50bdce97e8c19a60485c41
viktmv <viktmv@icloud.com>
2017-05-30 17:32:18 +0000

    render messages form data with unique key

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,37 @@ import MessageList from './MessageList.jsx'
 import Nav from './Nav.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ],
+      counter: 0
+    }
+    this.newID = this.newID.bind(this);
+  }
+  newID() {
+    return this.state.counter++
+  }
+
   render() {
+
+    let {name} = this.state.currentUser
     return (
       <div>
         <Nav></Nav>
-        <MessageList></MessageList>
-        <ChatBar></ChatBar>
+        <MessageList messages={this.state.messages} newID={this.newID}></MessageList>
+        <ChatBar user={name}></ChatBar>
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,9 +3,15 @@ import Message from './Message.jsx'
 
 class MessageList extends Component {
   render() {
+
+    let {messages} = this.props
+    let {newID }= this.props
     return (
       <main className="messages">
-          <Message></Message>
+          {messages.map(message => {
+
+            return <Message key={newID()} name={message.username} content={message.content}></Message>
+          })}
           <div className="message system">
             Anonymous1 changed their name to nomnom.
           </div>
