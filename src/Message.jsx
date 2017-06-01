5a8e2aa322d73546c61dd44d45815e4db8a46176
viktmv <viktmv@icloud.com>
2017-06-01 00:49:49 +0000

    Add img support

--- src/Message.jsx
+++ src/Message.jsx
@@ -11,7 +11,7 @@ class Message extends Component {
       return (
         <div className="message">
           <span className={`message-username ${this.props.colour}`}>{this.props.name}</span>
-          <span className="message-content">{this.props.content}</span>
+          <span className="message-content">{this.imgCheck()}</span>
         </div>
       )
     else return (
@@ -19,6 +19,12 @@ class Message extends Component {
           {this.props.content}
         </div>
       )
+  }
+
+  imgCheck() {
+    return /\.(jpe?g)|(png)|(gif)$/gi.test(this.props.content)
+    ? <img src={this.props.content} style={{width: '60%'}} />
+    : this.props.content
   }
 }
 
